c37170c49dbed86812f72bf8ec675cedda294e59
Dian Gunawan <digoens@gmail.com>
2017-07-28 18:56:02 +0700

    latest update

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,17 +10,19 @@ $(document).ready(function(){
 	    {
 	      breakpoint: 1024,
 	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
+	        slidesToShow: 4,
+	        slidesToScroll: 4,
 	        infinite: true,
 	        dots: true
 	      }
 	    },
 	    {
-	      breakpoint: 600,
+	      breakpoint: 960,
 	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: true
 	      }
 	    },
 	    {
@@ -30,9 +32,6 @@ $(document).ready(function(){
 	        slidesToScroll: 1
 	      }
 	    }
-	    // You can unslick at a given breakpoint now by adding:
-	    // settings: "unslick"
-	    // instead of a settings object
 	  ]
 	});
 });
